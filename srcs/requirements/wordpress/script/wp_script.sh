933090e2ae3e25109b9b5f1648b2ca7d6fd3ba09
Benedikt Hielscher <113378646+benemanu@users.noreply.github.com>
2023-11-20 13:49:17 +0000

    1

--- srcs/requirements/wordpress/script/wp_script.sh
+++ srcs/requirements/wordpress/script/wp_script.sh
@@ -1,17 +1,26 @@
-
-mkdir /var/www/
-mkdir /var/www/html
-
-cd /var/www/html
-
-rm -rf *
+rm -rf
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
 chmod +x wp-cli.phar
-
 mv wp-cli.phar /usr/local/bin/wp
 
 wp core download --allow-root
 
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+cp /wp-config.php /var/www/html/
+
+sed -i "s/database_name_here/$db_name/g" wp-config.php
+sed -i "s/username_here/$db_user/g" wp-config.php
+sed -i "s/password_here/$db_pass/g" wp-config.php
+sed -i "s/localhost/mariadb/g" wp-config.php
+
+mkdir -p /var/www/html
+cd /var/www/html
+wait-for-it.sh mariadb::3306 -t 60
+
+wp core install --url=$domain_name --title="Inception" --admin_user=$wp-admin --admin_password=$wp_adminpass --admin_email=$wp_adminmail --allow-root
+wp user create $wp_user  $wp_email --role=contributor --user_pass=$wp_pass --allow-root
+wp theme install twentytwenty --activate --allow-root
+wp plugin update -all -allow-root
+
+exec "$@"
